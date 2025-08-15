2a08d33f7e251105cb784fef2015de53757f9f14
crystalforceix <crystalforceix@gmail.com>
2025-08-15 09:42:48 +0700

    update ad test

---
+++ Scripts/rm-readmefiles.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rf ./README.md && rm -rf ./../README.md && rm -rf ./../BUILD/AUR-packages-Folder/README.md && rm -rf ./../BUILD/Storages/README.md && rm -rf ./../BUILD/Template-Built/out/README.md && rm -rf ./../BUILD/Template-Built/work/README.md
