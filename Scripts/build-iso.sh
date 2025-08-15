26d7024e76dc68a157eabca154b1d2e0f65128df
crystalforceix <crystalforceix@gmail.com>
2025-08-15 20:38:07 +0700

    add more scripts for helping make iso

---
+++ Scripts/build-iso.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd ./../BUILD/Template-Built && sudo mkarchiso -v -w work -o out ~/Projects/Cappa-Projects/ISO-Builder/Cappa-Linux/releng/
---
+++ Scripts/make-packages-database-in-local-repo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd ./../ISO-Builder/Cappa-Linux/releng/ && repo-add local/repo/custom.db.tar.gz local/repo/*.pkg.tar.zst 
