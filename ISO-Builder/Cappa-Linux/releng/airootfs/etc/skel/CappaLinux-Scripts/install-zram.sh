967f1544641816e005dcbd0cd24ff55dbaf5051f
crystalforceix <crystalforceix@gmail.com>
2025-08-27 20:58:32 +0700

    update calla packages

--- ISO-Builder/Cappa-Linux/releng/airootfs/etc/skel/CappaLinux-Scripts/install-zram.sh
+++ ISO-Builder/Cappa-Linux/releng/airootfs/etc/skel/CappaLinux-Scripts/install-zram.sh
@@ -4,7 +4,7 @@ echo "let's me install zram-generator tool."
 sudo pacman -Syu zram-generator
 echo "now i'll setup zram for ya:D"
 sudo touch /usr/lib/systemd/zram-generator.conf
-echo -e "[zram0]\nzram-size=ram\ncompression-algorithm=zstd\nswap-priority=60" >> /usr/lib/systemd/zram-generator.conf
+sudo echo -e "[zram0]\nzram-size=ram\ncompression-algorithm=zstd\nswap-priority=60" >> /usr/lib/systemd/zram-generator.conf
 sudo systemctl daemon-reload && sudo systemctl start /dev/zram0
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 echo "Done! but to prevent conflicts with zram pls in the line locate GRUB_CMDLINE_LINUX_DEFAULT add in the end 'zswap.enabled=0'"
