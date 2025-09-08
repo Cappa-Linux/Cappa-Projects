3c2651a9d97c54c6f72dbee801dcae7da88bf304
crystalforceix <crystalforceix@gmail.com>
2025-09-08 14:10:15 +0700

    add mkinitcpio for kernel zen scripts

--- ISO-Builder/Cappa-Linux/releng/airootfs/etc/skel/CappaLinux-Scripts/remove-archiso-for-mkinitcpio.sh
+++ ISO-Builder/Cappa-Linux/releng/airootfs/etc/skel/CappaLinux-Scripts/remove-archiso-for-mkinitcpio.sh
@@ -16,7 +16,7 @@ sudo rm -rf /etc/mkinitcpio.d/linux-zen.preset
 sleep 0.5
 echo "Done! let's me copy paste new mkinitcpio config of kernel zen"
 sleep 1
-sudo cp -r ~/CappaLinux-Scripts/Storages/new-mkinitcpio-config-zen/linux-zen.preset
+sudo cp -r ~/CappaLinux-Scripts/Storages/new-mkinitcpio-config-zen/linux-zen.preset /etc/mkinitcpio.d/
 sleep 1
 echo "Hold on i need refresh mkinitcpio config or kernel zen..."
 sudo mkinitcpio -P
